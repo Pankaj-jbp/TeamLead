5e54976b0ce012c8475bad4642b51899787a039e
rahul <rkrk121@gmail.com>
2024-06-09 13:34:45 +0530

    commited by rahul

---
+++ com.qsp.Trello/src/test/java/com/qsp/Trello/facebook/A.java
@@ -0,0 +1,16 @@
+package com.qsp.Trello.facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A {
+
+	public static void main(String[] args) {
+		WebDriver driver  = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		//Changes by Rahul
+		
+
+	}
+
+}
