7b6c1e3a513cd844393ebb5555725ca875e0749f
rahul <rkrk121@gmail.com>
2024-06-09 14:20:29 +0530

    Merge branch 'master' of https://github.com/rkrk121/TeamLead.git

--- com.qsp.Trello/src/test/java/com/qsp/Trello/facebook/A.java
+++ com.qsp.Trello/src/test/java/com/qsp/Trello/facebook/A.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class A {
 
 	public static void main(String[] args) {
-		WebDriver driver  = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		//Changes by Rahul
+		//he;llow
 		
 
 	}
